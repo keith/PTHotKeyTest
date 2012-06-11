201ddfed11fbcf90c9e3ac4d9c568e47b04bc65a
Keith Smiley <keithbodesmiley@gmail.com>
2012-06-11 08:09:19 -0400

    Initial Commit

---
+++ PTHotKeyTest/KSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KSAppDelegate.h
+//  PTHotKeyTest
+//
+//  Created by Keith Smiley on 6/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface KSAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ PTHotKeyTest/KSAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  KSAppDelegate.m
+//  PTHotKeyTest
+//
+//  Created by Keith Smiley on 6/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "KSAppDelegate.h"
+
+@implementation KSAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
